cd6b6b518db92899a116588bc1ad8af82175b88f
michal.lipski <michal.lipski@newvoicemedia.com>
2018-06-26 02:34:34 +0200

    some

--- src/timer/timer.spec.js
+++ src/timer/timer.spec.js
@@ -1,13 +1,10 @@
 const timer = require('./timer');
 
-jest.useFakeTimers();
-
 test('calls the callback after 1 second', () => {
     const timer = require('./timer');
     const callback = jest.fn();
   
     timer.callWithDelay(callback);  
-    jest.runAllTimers();
   
     expect(callback).toBeCalled();
     expect(callback).toHaveBeenCalledTimes(1);
